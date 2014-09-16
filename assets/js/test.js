4685642cd16649d0f5111feca656677c15155d97
david <davidf1212@gmail.com>
2014-09-16 18:00:21 -0400

    fixed intro

--- assets/js/test.js
+++ assets/js/test.js
@@ -4,8 +4,12 @@ var xRingCenter = $(window).width() / 4;
 var yRingCenter = $(window).height() / 2;
 var ringRadius = 160;
 var circleCentered;
+var animationLength = 600;
 
 $(document).ready(function() {
+	$("#main-box").animate({
+		"left": "50%"
+	}, 1000);
 	$(".circle").click(function(event) {
 		var circle = $(this);
 		if (circle.attr("clicked") == 0) {
@@ -21,18 +25,20 @@ $(document).ready(function() {
 function setCirclePositions () {
 	for (var i = 1; i <= numCircles; i++) {
 		var circle = $("#circle-" + i);
-		var theta = (Math.PI * 2 * (i - 1) + Math.PI) / numCircles;
-		var x = xRingCenter + Math.cos(theta) * ringRadius
-	;
-		var y = yRingCenter + Math.sin(theta) * ringRadius
-	;
+		var theta = (Math.PI * 2 * (i - 1) - Math.PI * 3) / numCircles;
+		var x = xRingCenter + Math.cos(theta) * ringRadius;
+		var y = yRingCenter + Math.sin(theta) * ringRadius;
 		circle.attr("clicked", 0);
 		circle.attr("y-orig", y);
 		circle.attr("x-orig", x);
-		circle.animate({
-			"top": y,
-			"left": x
-		}, 600);
+		if (i == 1) {
+			circleEnter(circle);
+		} else {
+			circle.animate({
+				"top": y,
+				"left": x
+			}, animationLength);
+		}
 	}
 	// circleEnter($("#circle-1"));
 }
@@ -48,7 +54,7 @@ function circleEnter(circle) {
 		"margin-top": -w / 2,
 		"top": yRingCenter,
 		"left": xRingCenter
-	}, 600);
+	}, animationLength);
 	circle.attr("clicked", 1);
 }
 
@@ -61,6 +67,6 @@ function circleExit(circle) {
 		"margin-top": -w / 2,
 		"top": circle.attr('y-orig'),
 		"left": circle.attr('x-orig')
-	}, 600);
+	}, animationLength);
 	circle.attr("clicked", 0);
 }
