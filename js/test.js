45200cb6dac9948e924c2b7ff1c1848a48a38dfe
david <davidf1212@gmail.com>
2014-09-16 11:59:41 -0400

    custom font

--- js/test.js
+++ js/test.js
@@ -1,16 +1,13 @@
 var numCircles = 6;
 var xScreenCenter = 600;
 var yScreenCenter = 300;
-var circleRadius = 120;
+var circleRadius = 240;
 var circleCentered;
 
 $(document).ready(function() {
 	$(".circle").click(function(event) {
 		var circle = $(this);
 		if (circle.attr("clicked") === "0") {
-			// var other = $("circle[clicked='1']");
-			// console.log(other);
-			// circleExit(other);
 			if (circleCentered != null) {
 				circleExit(circleCentered);
 			}
@@ -22,7 +19,7 @@ $(document).ready(function() {
 
 function setCirclePositions () {
 	for (var i = 1; i <= numCircles; i++) {
-		var theta = Math.PI / numCircles + Math.PI * 2 * i / numCircles;
+		var theta = Math.PI * 3 / 2 + Math.PI * 2 * (i - 1) / numCircles / 3;
 		var circle = $("#circle-" + i);
 		var x = xScreenCenter + Math.cos(theta) * circleRadius;
 		var y = yScreenCenter + Math.sin(theta) * circleRadius;
@@ -39,6 +36,10 @@ function setCirclePositions () {
 function circleEnter(circle) {
 	circleCentered = circle;
 	circle.animate({
+		"width": 320,
+		"height": 320,
+		"margin-left": -160,
+		"margin-top": -160,
 		"top": yScreenCenter,
 		"left": xScreenCenter
 	}, 600);
@@ -47,6 +48,10 @@ function circleEnter(circle) {
 
 function circleExit(circle) {
 	circle.animate({
+		"width": 80,
+		"height": 80,
+		"margin-left": -40,
+		"margin-top": -40,
 		"top": circle.attr("y-orig"),
 		"left": circle.attr("x-orig")
 	}, 600);
