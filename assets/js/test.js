b7cc1bc5e582a8c14c765fdd803890e9b1cb6537
david <davidf1212@gmail.com>
2014-09-17 12:13:26 -0400

    mouse hover and images

--- assets/js/test.js
+++ assets/js/test.js
@@ -8,37 +8,46 @@ var animShort = 400;
 var animLong = 800;
 
 $(document).ready(function() {
-	$("#main-box").animate({
-		"left": "50%"
-	}, animLong);
-	$(".circle").click(function(event) {
+	loadItems();
+	$(".circle").hover(function () {
 		var circle = $(this);
-		if (circle.attr("clicked") == 0) {
+		circle.css('background-color', '#ffffcc');
+	}, function () {
+		var circle = $(this);
+		circle.css('background-color', '#22a044');
+	});
+
+	$('.circle').click(function() {
+		var circle = $(this);
+		if (circle.attr('clicked') == 0) {
 			if (circleCentered != null) {
 				circleExit(circleCentered);
 			}
 			circleEnter(circle);
 		}
-		$("#main-box").attr("href");
+		$('#main-box').attr('href');
 	});
-	setCirclePositions();
 });
 
-function setCirclePositions () {
+function loadItems () {
+	$('#main-box').animate({
+		'left': '50%'
+	}, animLong);
+
 	for (var i = 1; i <= numCircles; i++) {
-		var circle = $("#circle-" + i);
+		var circle = $('#circle-' + i);
 		var theta = (Math.PI * 2 * (i - 1) - Math.PI * 3) / numCircles;
 		var x = xRingCenter + Math.cos(theta) * ringRadius;
 		var y = yRingCenter + Math.sin(theta) * ringRadius;
-		circle.attr("clicked", 0);
-		circle.attr("y-orig", y);
-		circle.attr("x-orig", x);
+		circle.attr('clicked', 0);
+		circle.attr('y-orig', y);
+		circle.attr('x-orig', x);
 		if (i == 1) {
 			circleEnter(circle);
 		} else {
 			circle.animate({
-				"top": y,
-				"left": x
+				'top': y,
+				'left': x
 			}, animShort);
 		}
 	}
@@ -48,29 +57,29 @@ function circleEnter(circle) {
 	circleCentered = circle;
 	var w = ringRadius * 2 - 140;
 	circle.animate({
-		"width": w,
-		"height": w,
-		"line-height": w,
-		"margin-left": -w / 2,
-		"margin-top": -w / 2,
-		"top": yRingCenter,
-		"left": xRingCenter
-	}, animShort, "swing", function() {	});
-	circle.attr("clicked", 1);
-	$(".panel-text").hide();
-	$("#" + circle.attr("name")).show();
+		'width': w,
+		'height': w,
+		'line-height': w,
+		'margin-left': -w / 2,
+		'margin-top': -w / 2,
+		'top': yRingCenter,
+		'left': xRingCenter
+	}, animShort, 'swing', function() {	});
+	circle.attr('clicked', 1);
+	$('.panel-text').hide();
+	$('#' + circle.attr('name')).show();
 }
 
 function circleExit(circle) {
 	var w = 100;
 	circle.animate({
-		"width": w,
-		"height": w,
-		"line-height": w,
-		"margin-left": -w / 2,
-		"margin-top": -w / 2,
-		"top": circle.attr('y-orig'),
-		"left": circle.attr('x-orig')
-	}, animShort, "swing", function() { });
-	circle.attr("clicked", 0);
+		'width': w,
+		'height': w,
+		'line-height': w,
+		'margin-left': -w / 2,
+		'margin-top': -w / 2,
+		'top': circle.attr('y-orig'),
+		'left': circle.attr('x-orig')
+	}, animShort, 'swing', function() { });
+	circle.attr('clicked', 0);
 }
