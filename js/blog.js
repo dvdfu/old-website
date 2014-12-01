9b8ef69171d48d13de6bd03bb737533448b8bb2d
david <davidf1212@gmail.com>
2014-12-01 13:30:23 -0500

    added new project and artworks

--- js/blog.js
+++ js/blog.js
@@ -1,23 +1,29 @@
 var APIblog = 'dvdfu.tumblr.com';
 var APIkey = 'Zj8p3vYT7tfYAvk22I80NeoHk4q59Fc7OOQRZAuxz1pDyCO7o4';
-var APIurl = 'http://api.tumblr.com/v2/blog/' + APIblog + '/posts?api_key=' + APIkey + '&limit=6';
+var APIurl = 'http://api.tumblr.com/v2/blog/' + APIblog + '/posts?api_key=' + APIkey + '&limit=4';
+var blogClicked = 0;
 
 $(document).ready(function() {
-	$.ajax({
-	    url: APIurl,
-	    dataType: 'jsonp',
-	    success: function(tumblr) {
-	    	var posts = tumblr.response.posts;
-	        for (var i in posts) {
-	        	$('.blogfeed').append('<h3 href=' + posts[i].short_url + '>' + posts[i].date.substr(0, 10) + '</h3>');
-	        	for (var j in posts[i].photos) {
-	        		$('.blogfeed').append('<a href="' + posts[i].photos[j].original_size.url +
-						'" data-lightbox="post' + i + '"><img class="project-images" src="' +
-						posts[i].photos[j].original_size.url + '"></a>');
-	        	}
-	        	$('.blogfeed').append(posts[i].caption);
-	        	$('.blogfeed').append('<div class="divider"></div>');
-	        }
-	    }
+	$('.circle-7').click(function() {
+		if (blogClicked == 0) {
+			blogClicked = 1;
+			$.ajax({
+			    url: APIurl,
+			    dataType: 'jsonp',
+			    success: function(tumblr) {
+			    	var posts = tumblr.response.posts;
+			        for (var i in posts) {
+			        	$('.blogfeed').append('<h3 href=' + posts[i].short_url + '>' + posts[i].date.substr(0, 10) + '</h3>');
+			        	for (var j in posts[i].photos) {
+			        		$('.blogfeed').append('<a href="' + posts[i].photos[j].original_size.url +
+								'" data-lightbox="post' + i + '"><img class="project-images" src="' +
+								posts[i].photos[j].original_size.url + '"></a>');
+			        	}
+			        	$('.blogfeed').append(posts[i].caption);
+			        	$('.blogfeed').append('<div class="divider"></div>');
+			        }
+			    }
+			});
+		}
 	});
 });
